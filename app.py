401da48c5f8a5f44dec1f37a3ef812adbc7addf0
Eren Aydoslu <erenaydoslue@icloud.com>
2024-05-14 22:01:14 +0200

    Changed URL

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-MODEL_SERVICE_URL = "localhost:5000"
+MODEL_SERVICE_URL = "127.0.0.1:5000"
 
 
 def handle_prediction_request(url):
